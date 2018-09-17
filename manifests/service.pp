6fff13874d4b83ff89c89bec5541172433eda246
Anne Teja <anneteja49@gmail.com>
2018-09-17 23:16:35 +0000

    fourteen

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,7 @@
 class apache::service (
-	String $servicename = lookup({"name" => "apache.servicename", "default_value" => $::apache::parameters::servicename})
-	) inherits ::apache::parameters {
+	String $servicename = lookup({'name' => 'apache.servicename', 'default_value' => $::apache::parameters::servicename})
+	)
+        inherits ::apache::parameters {
 	service { 'apache-service':
 		ensure     => running,
 		name       => $servicename,
