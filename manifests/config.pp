8063506762cbe1d18b6ae652c2c767c00bfa9264
Anne Teja <anneteja49@gmail.com>
2018-09-26 20:20:25 +0000

    dissatisfied

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,12 +5,12 @@ class apache::config(
 	String $configfile   = lookup ({'name'   => 'apache.configfile',  'default_value'   => $::apache::parameters::configfile}),
 	String $vhostsource  = lookup ({'name'   => 'apache.vhostsource', 'default_value'   => $::apache::parameters::vhostsource}),
 	String $vhostfile    = lookup ({'name'   => 'apache.vhostfile',  'default_value'   => $::apache::parameters::vhostfile}),
-	) 
-      inherits ::apache::parameters {
-	                           file { 'config-file':
-		                    ensure => file,
-		                    path   => $configfile,
-		                    source => $configsource,
+   ) 
+   inherits ::apache::parameters {
+	                       file { 'config-file':
+		                ensure => file,
+		                path   => $configfile,
+		                source => $configsource,
                    }
 
 	        file { 'vhost-file':
