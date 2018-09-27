824c6a5dcaa4eba19e6f96b8a0f8ddf95b5b4f74
Anne Teja <anneteja49@gmail.com>
2018-09-27 20:54:13 +0000

    change in parameters

--- manifests/parameters.pp
+++ manifests/parameters.pp
@@ -1,12 +1,13 @@
-class apache::parameters {
-	                  if $::osfamily == 'RedHat' {
+# 
+class apache::parameters{
+	                  if $::osfamily == 'RedHat'{
 		                                      $packagename  = 'httpd'
     		                                      $configfile   = '/etc/httpd/conf/httpd.conf'
     		                                      $configsource = 'puppet:///modules/apache/httpd.conf'
 		                                      $vhostfile    = '/etc/httpd/conf.d/vhost.conf'
 		                                      $vhostsource  = 'apache/vhost.conf.erb'
   		                                      $servicename  = 'httpd'
-                                               } elsif $::osfamily == 'Debian' {
+                                               } elsif $::osfamily == 'Debian'{
 		                                                                $packagename  = 'apache2'
     		                                                                $configfile   = '/etc/apache2/apache2.conf'
     		                                                                $configsource = 'puppet:///modules/apache/apache2.conf'
