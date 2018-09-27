6daced1ce7021b0e994a0a9e60d52e9d4990cf39
Anne Teja <anneteja49@gmail.com>
2018-09-27 21:34:48 +0000

    still no luck

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,8 +2,8 @@
 class apache::package(
 	              String $packagename = lookup ({'name' => 'apache.packagename', 'default_value' => $::apache::parameters::packagename})
 	          )  
-                  inherits ::apache::parameters{
-	                                        package{'apache-package':
+                   inherits ::apache::parameters{
+	                                        package{ 'apache-package':
 		                                         ensure => present,
 		                                         name   => $packagename,
                                                    }
--- manifests/service.pp
+++ manifests/service.pp
@@ -3,7 +3,7 @@ class apache::service(
 	              String $servicename = lookup ({'name' => 'apache.servicename', 'default_value' => $::apache::parameters::servicename})
 	         )
                   inherits ::apache::parameters{
-	                                        service{'apache-service':
+	                                        service{ 'apache-service':
 		                                         ensure     => running,
 		                                         name       => $servicename,
 		                                         enable     => true,
