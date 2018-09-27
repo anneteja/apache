897d3301f048aa08b51079d7d02b1403bdd84bff
Anne Teja <anneteja49@gmail.com>
2018-09-27 21:41:49 +0000

    what to do

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,9 +2,9 @@
 class apache::package(
 	              String $packagename = lookup ({'name' => 'apache.packagename', 'default_value' => $::apache::parameters::packagename})
 	          )  
-                   inherits ::apache::parameters{
-	                                        package{ 'apache-package':
-		                                         ensure => present,
-		                                         name   => $packagename,
-                                                   }
-                                          }   
+                  inherits ::apache::parameters {
+	                                         package { 'apache-package':
+		                                           ensure => present,
+		                                           name   => $packagename,
+                                                     }
+                                            }    
