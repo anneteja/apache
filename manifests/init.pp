19d15ef5b2ffde12b51b2fed50aa9483c1dc0003
Anne Teja <anneteja49@gmail.com>
2018-09-27 20:35:46 +0000

    changes in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,8 @@
 # Copyright 2018 Your name here, unless otherwise noted.
 #
 class apache{
-	    include apache::package
-	    include apache::config
-	    include apache::service
-
-	    Class['apache::package'] -> Class['apache::config'] ~> Class['apache::service']
-        }
+	     include apache::package
+	     include apache::config
+	     include apache::service
+             Class['apache::package'] -> Class['apache::config'] ~> Class['apache::service']
+     }
